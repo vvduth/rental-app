4c10979d84e63e0ac86fce92123c24694f12d60e
vvduth <ducthai060501@gmail.com>
2025-06-05 11:12:57 +0300

    feat: update ApplicationPage layout; enhance subtitle for clarity and improve application status display

--- client/app/(dashboard)/tenants/applications/page.tsx
+++ client/app/(dashboard)/tenants/applications/page.tsx
@@ -40,57 +40,49 @@ const ApplicationPage = () => {
     return <div>Error loading applications</div>;
   }
 
-  return (
-    <div className="p-5">
+   return (
+    <div className="dashboard-container">
       <Header
         title="Applications"
-        subtitle="Track and manage all applications"
+        subtitle="Track and manage your property rental applications"
       />
-      return (
-      <div className="dashboard-container">
-        <Header
-          title="Applications"
-          subtitle="Track and manage your property rental applications"
-        />
-        <div className="w-full">
-          {applications?.map((application: Application) => (
-            <ApplicationCard
-              key={application.id}
-              application={application}
-              userType="renter"
-            >
-              <div className="flex justify-between gap-5 w-full pb-4 px-4">
-                {application.status === "Approved" ? (
-                  <div className="bg-green-100 p-4 text-green-700 grow flex items-center">
-                    <CircleCheckBig className="w-5 h-5 mr-2" />
-                    The property is being rented by you until{" "}
-                    {new Date(application.lease?.endDate).toLocaleDateString()}
-                  </div>
-                ) : application.status === "Pending" ? (
-                  <div className="bg-yellow-100 p-4 text-yellow-700 grow flex items-center">
-                    <Clock className="w-5 h-5 mr-2" />
-                    Your application is pending approval
-                  </div>
-                ) : (
-                  <div className="bg-red-100 p-4 text-red-700 grow flex items-center">
-                    <XCircle className="w-5 h-5 mr-2" />
-                    Your application has been denied
-                  </div>
-                )}
+      <div className="w-full">
+        {applications?.map((application:Application) => (
+          <ApplicationCard
+            key={application.id}
+            application={application}
+            userType="renter"
+          >
+            <div className="flex justify-between gap-5 w-full pb-4 px-4">
+              {application.status === "Approved" ? (
+                <div className="bg-green-100 p-4 text-green-700 grow flex items-center">
+                  <CircleCheckBig className="w-5 h-5 mr-2" />
+                  The property is being rented by you until{" "}
+                  {new Date(application.lease?.endDate).toLocaleDateString()}
+                </div>
+              ) : application.status === "Pending" ? (
+                <div className="bg-yellow-100 p-4 text-yellow-700 grow flex items-center">
+                  <Clock className="w-5 h-5 mr-2" />
+                  Your application is pending approval
+                </div>
+              ) : (
+                <div className="bg-red-100 p-4 text-red-700 grow flex items-center">
+                  <XCircle className="w-5 h-5 mr-2" />
+                  Your application has been denied
+                </div>
+              )}
 
-                <button
-                  className={`bg-white border border-gray-300 text-gray-700 py-2 px-4
+              <button
+                className={`bg-white border border-gray-300 text-gray-700 py-2 px-4
                           rounded-md flex items-center justify-center hover:bg-primary-700 hover:text-primary-50`}
-                >
-                  <Download className="w-5 h-5 mr-2" />
-                  Download Agreement
-                </button>
-              </div>
-            </ApplicationCard>
-          ))}
-        </div>
+              >
+                <Download className="w-5 h-5 mr-2" />
+                Download Agreement
+              </button>
+            </div>
+          </ApplicationCard>
+        ))}
       </div>
-      );
     </div>
   );
 };
