637b7abc07f964e679464fb19e4c4048f3ff9d50
vvduth <ducthai060501@gmail.com>
2025-06-05 14:48:48 +0300

    fix: update image source in SingleListing component to use photoUrls with fallback

--- client/app/(nondashboard)/search/[id]/page.tsx
+++ client/app/(nondashboard)/search/[id]/page.tsx
@@ -20,7 +20,7 @@ const SingleListing = () => {
   return (
     <div>
       <ImagePreviews
-        images={propertyData.imagesUrl}
+        images={propertyData.photoUrls || ["/singlelisting-2.jpg", "/singlelisting-3.jpg"]}
       />
       <div
         className="flex flex-col md:flex-row justify-center gap-10 mx-10 md:w-2/3 md:mx-auto
